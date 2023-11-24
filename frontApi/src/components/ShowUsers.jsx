ed4ecb152bea6e93feaafdeb0bfb4cc69991cdf4
Emmanuel <emmanuelescobedo0999@gmail.com>
2023-11-24 15:44:03 -0600

    modified:   frontApi/src/components/ShowUsers.jsx

--- frontApi/src/components/ShowUsers.jsx
+++ frontApi/src/components/ShowUsers.jsx
@@ -1,63 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import '../ShowUsers.css'; // Importa el archivo de estilos CSS
+// ShowUsers.jsx
+
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import '../ShowUsers.css'// Importa el archivo de estilos CSS
 
 export const ShowUsers = () => {
-    const URL = 'https://jsonplaceholder.typicode.com/users';
-    const [users, setUsers] = useState([]);
-    const [filteredUsers, setFilteredUsers] = useState([]);
-    const [detailsUser, setDetailsUser] = useState(null);
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
-    const [filter, setFilter] = useState('');
-    const [currentPage, setCurrentPage] = useState(1);
-    const usersPerPage = 5;
+    const URL = 'https://jsonplaceholder.typicode.com/users'
+    const [users, setUsers] = useState([])
+    const [filteredUsers, setFilteredUsers] = useState([])
+    const [detailsUser, setDetailsUser] = useState(null)
+    const [lastViewed, setLastViewed] = useState([])
+    const [filter, setFilter] = useState('')
+    const [currentPage, setCurrentPage] = useState(1)
+    const usersPerPage = 5
 
     useEffect(() => {
-        getUsers();
-    }, []);
+        getUsers()
+    }, [])
 
     useEffect(() => {
-        filterUsers();
-    }, [users, filter]);
+        filterUsers()
+    }, [users, filter])
 
     const getUsers = async () => {
-        const res = await axios.get(URL);
-        setUsers(res.data);
-    };
+        const res = await axios.get(URL)
+        setUsers(res.data)
+    }
 
     const filterUsers = () => {
         const filtered = users.filter((user) => {
-            const searchTerms = [user.name, user.username, user.email].map(term => term.toLowerCase());
-            return searchTerms.some(term => term.includes(filter.toLowerCase()));
-        });
-        setFilteredUsers(filtered);
-    };
+            const searchTerms = [user.name, user.username, user.email].map((term) =>
+                term.toLowerCase()
+            )
+            return searchTerms.some((term) => term.includes(filter.toLowerCase()))
+        })
+        setFilteredUsers(filtered)
+    }
 
     const clearFilter = () => {
-        setFilter('');
-    };
+        setFilter('')
+    }
 
-    const getDetailsUsers = async (userId) => {
+    const getDetailsUser = async (userId) => {
         try {
-            const res = await axios.get(URL + '/' + userId);
-            setDetailsUser(res.data);
-            setIsPopupOpen(true); // Abre el popup cuando se obtienen los detalles
+            const res = await axios.get(URL + '/' + userId)
+            setDetailsUser(res.data)
+            updateLastViewed(res.data) // Agregar el registro actual a la lista de últimos vistos
         } catch (error) {
-            console.error('Error fetching details:', error);
+            console.error('Error fetching details:', error)
         }
-    };
-
-    const closePopup = () => {
-        setDetailsUser(null); // Limpia los detalles al cerrar el popup
-        setIsPopupOpen(false);
-    };
+    }
 
     // Paginación
-    const indexOfLastUser = currentPage * usersPerPage;
-    const indexOfFirstUser = indexOfLastUser - usersPerPage;
-    const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+    const indexOfLastUser = currentPage * usersPerPage
+    const indexOfFirstUser = indexOfLastUser - usersPerPage
+    const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser)
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
+    const updateLastViewed = (user) => {
+        // Mantén solo los últimos 5 registros vistos
+        setLastViewed((prevLastViewed) => {
+            const updatedLastViewed = [user, ...prevLastViewed.slice(0, 4)]
+            return updatedLastViewed
+        })
+    }
 
     return (
         <div className='container'>
@@ -96,7 +103,7 @@ export const ShowUsers = () => {
                                         <button
                                             className='btn btn-info'
                                             style={{ marginRight: '10px' }}
-                                            onClick={() => getDetailsUsers(user.id)}
+                                            onClick={() => getDetailsUser(user.id)}
                                         >
                                             <i className='fas fa-info-circle'></i>
                                         </button>
@@ -107,43 +114,51 @@ export const ShowUsers = () => {
                     </table>
                     {/* Paginación */}
                     <ul className='pagination'>
-                        {Array.from({ length: Math.ceil(filteredUsers.length / usersPerPage) }, (_, index) => (
-                            <li key={index} className='page-item'>
-                                <button className='page-link' onClick={() => paginate(index + 1)}>
-                                    {index + 1}
-                                </button>
-                            </li>
-                        ))}
+                        {Array.from(
+                            { length: Math.ceil(filteredUsers.length / usersPerPage) },
+                            (_, index) => (
+                                <li key={index} className='page-item'>
+                                    <button
+                                        className='page-link'
+                                        onClick={() => paginate(index + 1)}
+                                    >
+                                        {index + 1}
+                                    </button>
+                                </li>
+                            )
+                        )}
                     </ul>
                 </div>
             </div>
 
-            {/* Popup */}
-            {isPopupOpen && (
+            {/* Últimos 5 registros vistos */}
+            <div className='last-viewed-section'>
+                <h2>Last 5 Viewed Users</h2>
+                <ul>
+                    {lastViewed.map((user) => (
+                        <li key={user.id}>
+                            {user.name} ({user.email})
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            {/* Detalles del Usuario */}
+            {detailsUser && (
                 <div className='popup'>
                     <div className='popup-content'>
-                        <span className='popup-close' onClick={closePopup}>
+                        <span className='popup-close' onClick={() => setDetailsUser(null)}>
                             &times;
                         </span>
                         <h3>User Details</h3>
-                        {detailsUser && (
-                            <>
-                                <p>Id: {detailsUser.id}</p>
-                                <p>Name: {detailsUser.name}</p>
-                                <p>Username: {detailsUser.username}</p>
-                                <p>Email: {detailsUser.email}</p>
-
-                                <p>Street: {detailsUser.address.street}</p>
-                                <p>Suite: {detailsUser.address.suite}</p>
-                                <p>City: {detailsUser.address.city}</p>
-                                <p>Phone: {detailsUser.phone}</p>
-                                <p>Website: {detailsUser.website}</p>
-
-                            </>
-                        )}
+                        <p>Id: {detailsUser.id}</p>
+                        <p>Name: {detailsUser.name}</p>
+                        <p>Username: {detailsUser.username}</p>
+                        <p>Email: {detailsUser.email}</p>
+                        {/* Agrega más detalles según tus necesidades */}
                     </div>
                 </div>
             )}
         </div>
-    );
-};
+    )
+}
